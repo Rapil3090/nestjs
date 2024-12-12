3dc6a2b01ed2df8e2991567d8fa811e1613e56ec
RapiL <sjw3090@gmail.com>
2024-12-12 23:00:56 +0900

    Serialization 적용

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,15 +1,19 @@
+import { Exclude } from "class-transformer";
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from "typeorm";
 
 
 export class BaseTable {
 
     @CreateDateColumn()
+    @Exclude()
     createAt: Date;
 
     @UpdateDateColumn()
+    @Exclude()
     updatedAt: Date;
 
     @VersionColumn()
+    @Exclude()
     version: number;
 
 }

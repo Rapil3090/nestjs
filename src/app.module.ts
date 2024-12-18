f5451141eae668d4640e0af7be3871dbcdaef743
RapiL <sjw3090@gmail.com>
2024-12-18 22:20:34 +0900

    ResponseTimeInterceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,10 @@ import { User } from './user/entity/user.entity';
 import { join } from 'path';
 import { envVariableKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
+import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 
 @Module({
   imports: [
@@ -70,6 +71,10 @@ import { RBACGuard } from './auth/guard/rbac.guard';
   {
     provide: APP_GUARD,
     useClass: RBACGuard,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ResponseTimeInterceptor,
   }
 ]
 })

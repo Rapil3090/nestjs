b7e029eac6f6b62c2633da2ff05f72527aa610dd
RapiL <sjw3090@gmail.com>
2024-12-18 23:07:53 +0900

    Forbidden Exception Filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,10 +17,11 @@ import { User } from './user/entity/user.entity';
 import { join } from 'path';
 import { envVariableKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
 import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
+import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
 
 @Module({
   imports: [
@@ -75,7 +76,11 @@ import { ResponseTimeInterceptor } from './common/interceptor/response-time.inte
   {
     provide: APP_INTERCEPTOR,
     useClass: ResponseTimeInterceptor,
-  }
+  },
+  {
+    provide: APP_FILTER,
+    useClass: ForbiddenExceptionFilter,
+  },
 ]
 })
 export class AppModule implements NestModule{

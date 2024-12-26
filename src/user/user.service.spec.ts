f510ac2c33757893ddd21677b38841ba4371680e
RapiL <sjw3090@gmail.com>
2024-12-26 23:30:33 +0900

    remove 테스트코드

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -3,6 +3,7 @@ import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { NotFoundException } from '@nestjs/common';
+import { async } from 'rxjs';
 
 const mockUserRepository = {
   findOne: jest.fn(),
@@ -10,6 +11,7 @@ const mockUserRepository = {
   create: jest.fn(),
   find: jest.fn(),
   remove: jest.fn(),
+  delete: jest.fn(),
 }
 
 
@@ -76,7 +78,36 @@ describe('UserService', () => {
           id: 999,
         }
       })
+    });
   });
+
+  describe("remove", () => {
+    it('should delete a user by id', async () => {
+      const id = 999;
+
+      jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue({
+        id:1,
+      });
+
+      const result = await userService.remove(id);
+
+      expect(result).toEqual(id);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: {
+          id,
+        }
+      })
+    });
+    
+    it('should throw a NotFoundException if user to delete is not found', () => {
+      jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue(null);
+
+      expect(userService.remove(999)).rejects.toThrow(NotFoundException);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: {
+          id: 999,
+        }
+      })
+    });
   });
 });
-
